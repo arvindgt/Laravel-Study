fefacefabfd4cf7d0048d55e1831141b53aec749
Arvind Kushwaha <arvindkshwh939@gmail.com>
2018-06-16 17:59:58 +0530

    controller and methods

--- app/Http/Controllers/Article1Controller.php
+++ app/Http/Controllers/Article1Controller.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article1;
 
 class Article1Controller extends Controller
 {
-    //
+    public function index()
+    {
+        return Article1::all();
+    }
+
+    public function show(Article1 $article)
+    {
+        return $article;
+    }
+
+    public function store(Request $request)
+    {
+        //print_r($request->all());
+        //exit;
+        $article = Article1::create($request->all());
+        
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Article1 $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Article1 $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
 }
